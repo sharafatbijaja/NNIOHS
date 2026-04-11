f6d706c1e6e8a00aba8756b573dc00f1296da9d4
Sharafat Bijaja <sharafatbijaja@gmail.com>
2026-04-11 18:44:50 +0500

    port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 // cPanel automatically assigns a port or a socket path
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.prepare().then(() => {
   createServer((req, res) => {
